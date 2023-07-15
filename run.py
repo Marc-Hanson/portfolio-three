06a971d097bfc80e690d04a94d381f5880c4ac39
Marc-Hanson <>
2023-07-15 15:58:24 +0000

    Landing page and main game function added.

--- run.py
+++ run.py
@@ -1,3 +1,42 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def main_game():
+  """
+  Main game functions, will run in order as the game progresses.
+  """
+  choice_one()
+  choice_two()
+  choice_three()
+  choice_four()
+  choice_five()
+
+#ASCII Art landing page, while loop which requires 'ready' to escape.
+print("""
+                      ____|====|____
+     _______________/                \_______________ 
+   _|________________________________________________|_
+  |____________________________________________________|
+     
+  ,----------------------------------------------------,
+  | [][][][][]  [][][][][]  [][][][]  [][__]  [][][][] |
+  |                                                    |
+  |  [][][][][][][][][][][][][][_]    [][][]  [][][][] |
+  |  [_][][][][][][][][][][][][][ |   [][][]  [][][][] |
+  | [][_][][][][][][][][][][][][]||     []    [][][][] |
+  | [__][][][][][][][][][][][][__]    [][][]  [][][]|| |
+  |   [__][________________][__]              [__][]|| |
+  `----------------------------------------------------'
+""")
+print("Welcome to 'A day in the life', A text based adventure through")
+print("the life of a student. All you need to do is type out the")
+print("'keyword' highlighted in quotes to progress through the day.")
+print("You can also type 'quit' at any time to leave the game.")
+while True:
+  game_start = input("\nAre you 'ready' to wake up in the life of a coding student?\n")
+  game_start = game_start.lower()
+  if game_start == "ready":
+    break
+  elif game_start =="quit":
+    print("Not now... You didn't even start playing! Do you think that's funny?")
+  else:
+    print("That's ok, we can wait until you're 'ready'...\n")
+    
+main_game()
